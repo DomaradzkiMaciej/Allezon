f83e0f60263817ac8129e927e5b8e148603bcf90
Maciej Pióro <m@ciekpio.ro>
2023-07-18 15:08:58 +0200

    Update

--- run_all.sh
+++ run_all.sh
@@ -1,27 +1,29 @@
+#! /bin/bash
+
+pass=$1
+student=$2
+
+if [ $# -ne 2 ]; then
+    echo "Usage: $0 <password> <student>"
+    exit 1
+fi
+ 
 for i in $(seq -w 01 10); do
-    sshpass -p $1 ssh st108@st108vm1$i.rtb-lab.pl -o StrictHostKeyChecking=no -C "/bin/true";
+    sshpass -p $pass ssh "$student"@"$student"vm1"$i".rtb-lab.pl -o StrictHostKeyChecking=no -C "/bin/true";
     echo "Done"
 done
 
 sudo apt -y install ansible sshpass
 
-cd ansible || exit
+cd app_ansible || exit
 
-ansible-playbook -i ./hosts --extra-vars "ansible_user=st108 ansible_password=$1" docker-playbook.yaml
-ansible-playbook -i ./hosts --extra-vars "ansible_user=st108 ansible_password=$1" swarm-playbook.yaml
-ansible-playbook -i ./hosts --extra-vars "ansible_user=st108 ansible_password=$1" aerospike-playbook.yaml
+sed "s/_student_/$student/g" hosts.template > hosts
+
+ansible-playbook -i ./hosts --extra-vars "ansible_user=$student ansible_password=$pass" registry-playbook.yaml
+ansible-playbook -i ./hosts --extra-vars "ansible_user=$student ansible_password=$pass" docker-playbook.yaml
+ansible-playbook -i ./hosts --extra-vars "ansible_user=$student ansible_password=$pass" swarm-playbook.yaml
+ansible-playbook -i ./hosts --extra-vars "ansible_user=$student ansible_password=$pass" aerospike-playbook.yaml
 
 cd .. || exit
 
-DOCKER_CONFIG=${DOCKER_CONFIG:-$HOME/.docker}
-sudo mkdir -p $DOCKER_CONFIG/cli-plugins
-sudo curl -SL https://github.com/docker/compose/releases/download/v2.19.1/docker-compose-linux-x86_64 -o $DOCKER_CONFIG/cli-plugins/docker-compose
-sudo chmod +x $DOCKER_CONFIG/cli-plugins/docker-compose
-
-export PATH=~/usr/local/bin:$PATH
-
-sudo docker service create --name registry -p 5000:5000 registry:2
-sudo docker node update --label-add haproxy=true $(hostname)
-sudo docker-compose build
-sudo docker-compose push
-sudo docker stack deploy --compose-file docker-compose.yml allezon
+source ./run_docker.sh
---
+++ run_docker.sh
@@ -0,0 +1,19 @@
+#! /bin/bash
+
+# DOCKER_CONFIG=${DOCKER_CONFIG:-$HOME/.docker}
+# sudo mkdir -p $DOCKER_CONFIG/cli-plugins
+# sudo curl -SL https://github.com/docker/compose/releases/download/v2.19.1/docker-compose-linux-x86_64 -o $DOCKER_CONFIG/cli-plugins/docker-compose
+# sudo chmod +x $DOCKER_CONFIG/cli-plugins/docker-compose
+
+# export PATH=~/usr/local/bin:$PATH
+
+# sudo docker service create --name registry -p 5000:5000 registry:2
+
+for i in $(seq -w 01 10); do
+    sudo docker node update --label-add s"$i"=true st111vm1"$i"
+done
+
+sudo docker node update --label-add haproxy=true $(hostname)
+sudo docker compose build
+sudo docker compose push
+sudo docker stack deploy --compose-file docker-compose.yml allezon
