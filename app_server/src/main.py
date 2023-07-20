91ce98a04ebe2f916be9d8bed9beb9fcba970de1
Maciej Domaradzki <MaciejDomaradzki01@gmail.com>
2023-07-20 22:45:25 +0200

    fixing ordering

--- app_server/src/main.py
+++ app_server/src/main.py
@@ -3,6 +3,20 @@ from types_ import UserTag, UserProfile, Action
 
 from fastapi import FastAPI, Response
 import bisect
+from functools import total_ordering
+
+
+@total_ordering
+class Inversed:
+    def __init__(self, x):
+        self.x = x
+
+    def __eq__(self, other):
+        return self.x == other.x
+
+    def __lt__(self, other):
+        return other.x <= self.x
+
 
 aerospike_client = AerospikeClient()
 app = FastAPI()
@@ -15,7 +29,7 @@ def user_tags(user_tag: UserTag):
     for _ in range(3):
         user_profile, gen = aerospike_client.get_profile(user_tag.cookie)
         action_list = user_profile.buys if user_tag.action == Action.BUY else user_profile.views
-        bisect.insort(action_list, user_tag, key=lambda tag: -1 * tag.time)
+        bisect.insort(action_list, user_tag, key=lambda tag: Inversed(tag.time))
         del action_list[200:]
 
         if aerospike_client.put_profile(user_profile, gen):
