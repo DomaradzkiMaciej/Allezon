0c953021de49ed652e6aec34fe9bfff82e9d46cf
Maciej Domaradzki <MaciejDomaradzki01@gmail.com>
2023-07-24 18:16:51 +0200

    fixing automatic deployment

--- run_all.sh
+++ run_all.sh
@@ -7,13 +7,16 @@ if [ $# -ne 2 ]; then
     echo "Usage: $0 <student> <pass>"
     exit 1
 fi
- 
+
+sudo apt -y install sshpass docker ansible
+sudo add-apt-repository -y ppa:ansible/ansible
+sudo apt update
+sudo apt -y upgrade ansible
+
 for i in $(seq -w 01 10); do
     sshpass -p $pass ssh "$student"@"$student"vm1"$i".rtb-lab.pl -o StrictHostKeyChecking=no -C "/bin/true";
     echo "Done"
 done
-
-sudo apt -y install ansible sshpass
 
 cd app_ansible || exit
 
--- run_docker.sh
+++ run_docker.sh
@@ -1,11 +1,10 @@
 #! /bin/bash
 
-# DOCKER_CONFIG=${DOCKER_CONFIG:-$HOME/.docker}
-# sudo mkdir -p $DOCKER_CONFIG/cli-plugins
-# sudo curl -SL https://github.com/docker/compose/releases/download/v2.19.1/docker-compose-linux-x86_64 -o $DOCKER_CONFIG/cli-plugins/docker-compose
-# sudo chmod +x $DOCKER_CONFIG/cli-plugins/docker-compose
-
-# export PATH=~/usr/local/bin:$PATH
+ DOCKER_CONFIG=${DOCKER_CONFIG:-/usr/local/lib/docker}
+ sudo mkdir -p $DOCKER_CONFIG/cli-plugins
+ sudo curl -SL https://github.com/docker/compose/releases/download/v2.19.1/docker-compose-linux-x86_64 -o $DOCKER_CONFIG/cli-plugins/docker-compose
+ sudo chmod +x $DOCKER_CONFIG/cli-plugins/docker-compose
+ sudo usermod -a -G docker $USER
 
 for i in $(seq -w 01 06); do
     sudo docker node update --label-add s"$i"=true st108vm1"$i"
