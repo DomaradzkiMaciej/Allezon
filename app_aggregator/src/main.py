eaa4a0e19151d376e4233a67c7655d2ae4871803
Maciej Domaradzki <MaciejDomaradzki01@gmail.com>
2023-08-08 18:28:26 +0200

    fixing UsaCase 3

--- app_aggregator/src/main.py
+++ app_aggregator/src/main.py
@@ -12,7 +12,6 @@ aerospike_client = AerospikeClient()
 kafka_hosts = ['st108vm108.rtb-lab.pl:9092', 'st108vm109.rtb-lab.pl:9092', 'st108vm110.rtb-lab.pl:9092']
 
 admin_client = kafka.KafkaAdminClient(bootstrap_servers=kafka_hosts)
-consumer = kafka.KafkaConsumer(bootstrap_servers=kafka_hosts, group_id='aggregation')
 
 try:
     topic_list = [kafka.admin.NewTopic(name='aggregation', num_partitions=2, replication_factor=2)]
@@ -20,18 +19,20 @@ try:
 except kafka.errors.TopicAlreadyExistsError as err:
     pass
 
+consumer = kafka.KafkaConsumer('aggregation', bootstrap_servers=kafka_hosts, group_id='aggregation')
+
 
 def generate_buckets(user_tag: UserTag):
     action = user_tag.action.value
     time = user_tag.time.rsplit(':', 1)[0]
 
-    combinations = itertools.product([user_tag.origin, None], [user_tag.brand_id, None], [user_tag.category_id, None])
+    combinations = itertools.product([user_tag.origin, None], [user_tag.product_info.brand_id, None], [user_tag.product_info.category_id, None])
     return [f"{action}_{origin}_{brand_id}_{category_id}_{time}" for (origin, brand_id, category_id) in
             combinations]
 
 
 for msg in consumer:
-    user_tag = parse_obj_as(UserTag, json.loads(msg))
+    user_tag = parse_obj_as(UserTag, json.loads(msg.value))
 
     for bucket_name in generate_buckets(user_tag):
         for _ in range(3):
